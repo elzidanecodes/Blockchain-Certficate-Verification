7eadced7c27107c1db282f4cd184747e1d869546
Laita Zidane <laitazidane@gmail.com>
2025-03-04 08:42:28 +0700

    config server ganache

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,19 @@
 import os
 from dotenv import load_dotenv
+import json
+from web3 import Web3
 
 # Load environment variables
 load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 
 GANACHE_URL = os.getenv("GANACHE_URL")
 CONTRACT_ADDRESS = os.getenv("CONTRACT_ADDRESS")
-SECRET_KEY = os.getenv("SECRET_KEY")
+web3 = Web3(Web3.HTTPProvider(GANACHE_URL))
+
+
+# Cek koneksi
+if web3.is_connected():
+    print("Connected to Ganache")
+else:
+    print("Failed to connect to Ganache")
+    
