a471ad0d886dbd3a9348638255f5f749780c5a59
Laita Zidane <laitazidane@gmail.com>
2025-03-04 12:24:17 +0700

    main app

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 from routes.certificate import certificate_bp
+from routes.blockchain import blockchain_bp
+from routes.verification import verification_bp
+from flask_cors import CORS
 from routes.verification import verification_bp
 
 app = Flask(__name__)
+CORS(app)
 
-
+# Register Blueprints
+app.register_blueprint(certificate_bp, url_prefix="/certificate")
+app.register_blueprint(blockchain_bp, url_prefix="/blockchain")
+app.register_blueprint(verification_bp, url_prefix="/verification")
 
 @app.route("/")
 def index():
