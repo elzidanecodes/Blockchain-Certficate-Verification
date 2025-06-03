4bcc7cebed8b0c216788ad46b951a72a617dba89
Laita Zidane <laitazidane@gmail.com>
2025-06-03 13:30:24 +0700

    delete

--- backend/routes/frontend.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, render_template
-
-frontend_bp = Blueprint("frontend", __name__)
-
-@frontend_bp.route("/verify", methods=["GET"])
-def verify_page():
-    certificate_id = request.args.get("certificate_id", "")
-    return render_template("verify.html", certificate_id=certificate_id)
