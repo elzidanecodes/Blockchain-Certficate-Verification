14519790dd28a522c6d2e9d4e5d47e91e624558c
Laita Zidane <laitazidane@gmail.com>
2025-06-20 01:45:53 +0700

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
-// https://vite.dev/config/
+// Ambil cert dan key
+const key = fs.readFileSync(path.resolve(__dirname, '../backend/keys/key.pem'))
+const cert = fs.readFileSync(path.resolve(__dirname, '../backend/keys/cert.pem'))
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key,
+      cert
+    },
+    port: 5173
+  }
 })
